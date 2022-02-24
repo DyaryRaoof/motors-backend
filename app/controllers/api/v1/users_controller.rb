7af03f14001f686ff2afb37972e506498ff91440
DyaryRaoof <dyary2020@gmail.com>
2022-02-24 23:08:49 +0300

    Imlement simple login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
-          if @user.save
+        if @user.save
             render json: @user, status: :created
-          else
-            render json: @user.errors, status: :unprocessable_entity 
-          end
+        else
+            user = User.find_by(name: user_params[:name])
+            render json: {error: @user.errors, user: user}, status: :unprocessable_entity 
+        end
     end
 
     def user_params
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module MotorsBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
   end
 end
