59825683045129a4cd2aced28881f121eac3e7d8
DyaryRaoof <dyary2020@gmail.com>
2022-02-24 23:16:10 +0300

    FIx linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: @user, status: :created
-        else
-            user = User.find_by(name: user_params[:name])
-            render json: {error: @user.errors, user: user}, status: :unprocessable_entity 
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      user = User.find_by(name: user_params[:name])
+      render json: { error: @user.errors, user: user }, status: :unprocessable_entity
     end
+  end
 
-    def user_params
-        params.require(:user).permit(:name)
-    end
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
