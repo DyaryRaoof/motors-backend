c75c4a8c0d1f5bf874f2af45b8232cfa27892ae5
DyaryRaoof <dyary2020@gmail.com>
2022-02-24 21:06:30 +0300

    Added user registration api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+          if @user.save
+            render json: @user, status: :created
+          else
+            render json: @user.errors, status: :unprocessable_entity 
+          end
+    end
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+end
