e09194b28ff1e73010dbb7b91c315b3616188ba6
DyaryRaoof <dyary2020@gmail.com>
2022-02-25 20:34:12 +0300

    Fix linter errors

--- app/controllers/api/v1/motors_controller.rb
+++ app/controllers/api/v1/motors_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MotorsController < ApplicationController
-    def index
-        @motors = Motor.all
-        render json: @motors
-    end
-    
+  def index
+    @motors = Motor.all
+    render json: @motors
   end
+end
