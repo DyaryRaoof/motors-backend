03be8557f5776941e7530333cc38bf88f1d5eec5
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-03-08 12:50:54 +0300

    Add test for Addd and delete motors

--- spec/requests/motors_spec.rb
+++ spec/requests/motors_spec.rb
@@ -14,4 +14,23 @@ RSpec.describe 'motors', type: :request do
       expect(response.body).to include 'Motor 1'
     end
   end
+  
+  describe 'POST /motors' do
+    before(:example) { post '/api/v1/motors/create', params: { :name => 'Motor 10', :description => 'this is a motor', :price => 1000,
+                                 :image => 'https://i.ibb.co/JygvmW1/motor1.png' } }
+    it 'works! returns correct status code' do
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'delete /motors' do
+    before(:all) do
+      Rails.application.load_seed
+    end
+    before(:example) { delete '/api/v1/motors/1/destroy' }
+    it 'works! returns correct status code' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
 end
